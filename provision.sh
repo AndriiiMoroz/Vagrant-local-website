5a18287d4c75b2f68b9eca474f3cb93c4532c738
AndriiiMoroz <andriii.moroz.rob@gmail.com>
2024-07-19 22:08:24 +0300

    first commit

---
+++ provision.sh
@@ -0,0 +1,9 @@
+    sudo apt update -y
+    sudo apt-get install systemd
+    sudo apt install nginx -y
+    sudo apt install net-tools -y
+    #sudo yum install -y yum-utils
+    #sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+    #sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    #sudo systemctl start docker
+    #sudo docker run hello-world
